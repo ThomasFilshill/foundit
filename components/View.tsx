4dd945a72b3730db752b89c8f48c31885a968694
Thomas F. D. Filshill <tfilshill@tecnologiaunica.com.br>
2024-11-24 20:17:18 -0500

    fix(View): update view count after page load using unstable_after function

--- components/View.tsx
+++ components/View.tsx
@@ -1,6 +1,8 @@
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
 import Ping from "./Ping"
 import { client } from '@/sanity/lib/client';
+import { writeClient } from "@/sanity/lib/write-client";
+import { unstable_after as after } from 'next/server';
 
 
 const View = async ({ id }: { id: string }) => {
@@ -9,7 +11,13 @@ const View = async ({ id }: { id: string }) => {
     .withConfig({ useCdn: false })
     .fetch(STARTUP_VIEWS_QUERY, { id });
 
-    // TODO: update number of views
+    // It is not necessary to update the views before loading the page. We use the after function to update the views after the page has loaded.
+    after( async () => {
+        await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit();
+    });
 
     return <div className="view-container">
         <div className="absolute -top-2 -right-2">
